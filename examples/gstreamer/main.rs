8b79bc4cc60962233aa354ab8750f047cda3c280
Sebastian Dröge <sebastian@centricular.com>
2024-10-25 18:30:02 +0300

    gstreamer: Update to newer GStreamer bindings and clean up code

--- examples/gstreamer/main.rs
+++ examples/gstreamer/main.rs
@@ -1,50 +1,52 @@
 // Copyright © SixtyFPS GmbH <info@slint-ui.com>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-commercial
 
-use gstreamer::prelude::*;
-use gstreamer_gl::prelude::*;
+use gst::prelude::*;
+use gst_gl::prelude::*;
 
 slint::include_modules!();
 
 struct Player<C: slint::ComponentHandle + 'static> {
     app: slint::Weak<C>,
-    //pipeline: gstreamer::Pipeline,
-    pipeline: gstreamer::Element,
-    appsink: gstreamer_app::AppSink,
-    current_sample: std::sync::Arc<std::sync::Mutex<Option<gstreamer::Sample>>>,
-    gst_gl_context: Option<gstreamer_gl::GLContext>,
+    pipeline: gst::Pipeline,
+    appsink: gst_app::AppSink,
+    current_sample: std::sync::Arc<std::sync::Mutex<Option<gst::Sample>>>,
+    gst_gl_context: Option<gst_gl::GLContext>,
 }
 
 impl<C: slint::ComponentHandle + 'static> Player<C> {
     fn new(app: slint::Weak<C>) -> Result<Self, anyhow::Error> {
-        gstreamer::init()?;
+        gst::init()?;
 
-        let source = gstreamer::ElementFactory::make("playbin", None)?;
-        source.set_property("uri", "https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm");
-
-        let appsink = gstreamer::ElementFactory::make("appsink", None)?
-            .dynamic_cast::<gstreamer_app::AppSink>()
-            .unwrap();
-
-        appsink.set_property("enable-last-sample", false);
-        appsink.set_property("emit-signals", false);
-        appsink.set_property("max-buffers", 1u32);
-
-        let caps = gstreamer::Caps::builder("video/x-raw")
-            .features(&[&gstreamer_gl::CAPS_FEATURE_MEMORY_GL_MEMORY])
-            .field("format", gstreamer_video::VideoFormat::Rgba.to_str())
+        let caps = gst::Caps::builder("video/x-raw")
+            .features([gst_gl::CAPS_FEATURE_MEMORY_GL_MEMORY])
+            .field("format", gst_video::VideoFormat::Rgba.to_str())
             .field("texture-target", "2D")
+            .field("pixel-aspect-ratio", gst::Fraction::new(1, 1))
             .build();
-        appsink.set_caps(Some(&caps));
 
-        let glsink = gstreamer::ElementFactory::make("glsinkbin", None)?;
-        glsink.set_property("sink", &appsink);
+        let appsink = gst_app::AppSink::builder()
+            .name("appsink")
+            .enable_last_sample(false)
+            .max_buffers(1u32)
+            .caps(&caps)
+            .build();
 
-        source.set_property("video-sink", &glsink);
+        let glsink = gst::ElementFactory::make("glsinkbin").property("sink", &appsink).build()?;
+
+        let pipeline = gst::ElementFactory::make("playbin")
+            .property(
+                "uri",
+                "https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.webm",
+            )
+            .property("video-sink", glsink)
+            .build()?
+            .downcast::<gst::Pipeline>()
+            .unwrap();
 
         Ok(Self {
             app,
-            pipeline: source,
+            pipeline,
             appsink,
             current_sample: std::sync::Arc::new(std::sync::Mutex::new(None)),
             gst_gl_context: None,
@@ -62,27 +64,27 @@ impl<C: slint::ComponentHandle + 'static> Player<C> {
         };
 
         let (gst_gl_context, gst_gl_display) = unsafe {
-            let platform = gstreamer_gl::GLPlatform::EGL;
+            let platform = gst_gl::GLPlatform::EGL;
 
             let egl_display = egl.GetCurrentDisplay();
-            let display =
-                gstreamer_gl_egl::GLDisplayEGL::with_egl_display(egl_display as usize).unwrap();
+            let display = gst_gl_egl::GLDisplayEGL::with_egl_display(egl_display as usize).unwrap();
             let native_context = egl.GetCurrentContext();
 
             (
-                gstreamer_gl::GLContext::new_wrapped(
+                gst_gl::GLContext::new_wrapped(
                     &display,
                     native_context as _,
                     platform,
-                    gstreamer_gl::GLContext::current_gl_api(platform).0,
+                    gst_gl::GLContext::current_gl_api(platform).0,
                 )
                 .expect("unable to create wrapped GL context"),
                 display,
             )
         };
 
-        gst_gl_context.activate(true).expect("could not activate GSL GL context");
+        gst_gl_context.activate(true).expect("could not activate GStreamer GL context");
         gst_gl_context.fill_info().expect("failed to fill GL info for wrapped context");
+        gst_gl_context.activate(false).expect("could not deactivate GStreamer GL context");
 
         self.gst_gl_context = Some(gst_gl_context.clone());
 
@@ -91,23 +93,21 @@ impl<C: slint::ComponentHandle + 'static> Player<C> {
             let gst_gl_context = gst_gl_context.clone();
             move |_, msg| {
                 match msg.view() {
-                    gstreamer::MessageView::NeedContext(ctx) => {
+                    gst::MessageView::NeedContext(ctx) => {
                         let ctx_type = ctx.context_type();
-                        if ctx_type == *gstreamer_gl::GL_DISPLAY_CONTEXT_TYPE {
-                            if let Some(element) = msg
-                                .src()
-                                .map(|source| source.downcast::<gstreamer::Element>().unwrap())
+                        if ctx_type == *gst_gl::GL_DISPLAY_CONTEXT_TYPE {
+                            if let Some(element) =
+                                msg.src().and_then(|source| source.downcast_ref::<gst::Element>())
                             {
-                                let gst_context = gstreamer::Context::new(ctx_type, true);
+                                let gst_context = gst::Context::new(ctx_type, true);
                                 gst_context.set_gl_display(&gst_gl_display);
                                 element.set_context(&gst_context);
                             }
                         } else if ctx_type == "gst.gl.app_context" {
-                            if let Some(element) = msg
-                                .src()
-                                .map(|source| source.downcast::<gstreamer::Element>().unwrap())
+                            if let Some(element) =
+                                msg.src().and_then(|source| source.downcast_ref::<gst::Element>())
                             {
-                                let mut gst_context = gstreamer::Context::new(ctx_type, true);
+                                let mut gst_context = gst::Context::new(ctx_type, true);
                                 {
                                     let gst_context = gst_context.get_mut().unwrap();
                                     let structure = gst_context.structure_mut();
@@ -119,50 +119,43 @@ impl<C: slint::ComponentHandle + 'static> Player<C> {
                     }
                     _ => (),
                 }
-                // forward event
 
-                gstreamer::BusSyncReply::Pass
+                gst::BusSyncReply::Drop
             }
         });
-
-        self.pipeline.set_state(gstreamer::State::Playing).unwrap();
 
         let app_weak = self.app.clone();
 
         let current_sample_ref = self.current_sample.clone();
 
         self.appsink.set_callbacks(
-            gstreamer_app::AppSinkCallbacks::builder()
+            gst_app::AppSinkCallbacks::builder()
                 .new_sample(move |appsink| {
-                    let sample = appsink.pull_sample().unwrap();
-
-                    {
-                        //let buffer = sample.buffer().unwrap();
-                        let _info = sample
-                            .caps()
-                            .map(|caps| gstreamer_video::VideoInfo::from_caps(caps).unwrap())
-                            .unwrap();
-                    }
+                    let sample = appsink.pull_sample().map_err(|_| gst::FlowError::Flushing)?;
 
                     let current_sample_ref = current_sample_ref.clone();
 
-                    app_weak.upgrade_in_event_loop(move |app| {
-                        *current_sample_ref.lock().unwrap() = Some(sample);
+                    app_weak
+                        .upgrade_in_event_loop(move |app| {
+                            *current_sample_ref.lock().unwrap() = Some(sample);
 
-                        app.window().request_redraw();
-                    }).ok();
-                    Ok(gstreamer::FlowSuccess::Ok)
+                            app.window().request_redraw();
+                        })
+                        .ok();
+
+                    Ok(gst::FlowSuccess::Ok)
                 })
                 .build(),
-        )
+        );
+
+        self.pipeline.set_state(gst::State::Playing).unwrap();
     }
 }
 
 impl<C: slint::ComponentHandle + 'static> Drop for Player<C> {
     fn drop(&mut self) {
-        self.pipeline.send_event(gstreamer::event::Eos::new());
-        self.pipeline.set_state(gstreamer::State::Null).unwrap();
-        eprintln!("Player drop");
+        *self.current_sample.lock().unwrap() = None;
+        let _ = self.pipeline.set_state(gst::State::Null);
     }
 }
 
@@ -179,28 +172,28 @@ pub fn main() -> Result<(), anyhow::Error> {
                 player.setup_graphics(graphics_api);
             }
             slint::RenderingState::RenderingTeardown => {
-                todo!()
+                *player.current_sample.lock().unwrap() = None;
+                let _ = player.pipeline.set_state(gst::State::Null);
             }
             slint::RenderingState::BeforeRendering => {
                 if let Some(sample) = player.current_sample.lock().unwrap().as_ref() {
                     let buffer = sample.buffer_owned().unwrap();
                     let info = sample
                         .caps()
-                        .map(|caps| gstreamer_video::VideoInfo::from_caps(caps).unwrap())
+                        .map(|caps| gst_video::VideoInfo::from_caps(caps).unwrap())
                         .unwrap();
 
                     {
-                        // let sync_meta = buffer.meta::<gstreamer_gl::GLSyncMeta>().unwrap();
+                        // let sync_meta = buffer.meta::<gst_gl::GLSyncMeta>().unwrap();
                         // sync_meta.set_sync_point(player.gst_gl_context.as_ref().unwrap());
                     }
 
-                    if let Ok(frame) =
-                        gstreamer_video::VideoFrame::from_buffer_readable_gl(buffer, &info)
-                    {
-                        //let sync_meta = frame.buffer().meta::<gstreamer_gl::GLSyncMeta>().unwrap();
+                    if let Ok(frame) = gst_gl::GLVideoFrame::from_buffer_readable(buffer, &info) {
+                        //let sync_meta = frame.buffer().meta::<gst_gl::GLSyncMeta>().unwrap();
                         //sync_meta.wait(player.gst_gl_context.as_ref().unwrap());
 
-                        if let Some(texture) = frame.texture_id(0).and_then(|id| id.try_into().ok())
+                        if let Some(texture) =
+                            frame.texture_id(0).ok().and_then(|id| id.try_into().ok())
                         {
                             mw_weak.unwrap().set_texture(unsafe {
                                 slint::BorrowedOpenGLTextureBuilder::new_gl_2d_rgba_texture(
